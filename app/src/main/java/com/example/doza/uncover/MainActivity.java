d11efe8120895267d90c6d1557eb9614f00769bb
qdozaq <doza@indiginis.com>
2017-04-20 00:05:51 -0400

    Fix for AppCompatActivity

--- app/src/main/java/com/example/doza/uncover/MainActivity.java
+++ app/src/main/java/com/example/doza/uncover/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.doza.uncover;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
