7a93359dde0a8e22db144b1321600aed4f34118d
qdozaq <doza@indiginis.com>
2017-04-24 14:42:39 -0400

    initialize with twitter fabric code

--- app/src/main/java/com/example/doza/uncover/Food.java
+++ app/src/main/java/com/example/doza/uncover/Food.java
@@ -5,11 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
+
 public class Food extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "A0OSFhztiC2b5YBodx3URraw8";
+    private static final String TWITTER_SECRET = "lilg33TymSkVocqq2odImJBtjmclwJJCaMtnMPdVYk33oleuuq";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_food);
     }
     public void toMain(View v){
