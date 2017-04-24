b9e0500f6c0f17719eb413cbc6654fdf4cb7e7ab
qdozaq <doza@indiginis.com>
2017-04-24 14:59:47 -0400

    by glob it works..

--- app/src/main/java/com/example/doza/uncover/Food.java
+++ app/src/main/java/com/example/doza/uncover/Food.java
@@ -1,5 +1,6 @@
 package com.example.doza.uncover;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,10 +8,12 @@ import android.view.View;
 
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
+import com.twitter.sdk.android.tweetui.UserTimeline;
 
 import io.fabric.sdk.android.Fabric;
 
-public class Food extends AppCompatActivity {
+public class Food extends ListActivity {
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "A0OSFhztiC2b5YBodx3URraw8";
@@ -21,11 +24,15 @@ public class Food extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
-        setContentView(R.layout.activity_food);
-    }
-    public void toMain(View v){
-        Intent X = new Intent(this, MainActivity.class);
+        final UserTimeline userTimeline = new UserTimeline.Builder()
+                .screenName("PhreePhood")
+                .build();
+        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
+                .setTimeline(userTimeline)
+                .build();
 
-        startActivity(X);
+        setListAdapter(adapter);
+//        setContentView(R.layout.activity_food);
+
     }
 }
