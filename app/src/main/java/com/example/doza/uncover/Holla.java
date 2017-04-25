72114324bf8df80a3bbdb1827a0ae8ba2a35d6c9
Nina Joseph <nrjoseph@live.unc.edu.com>
2017-04-25 15:59:03 -0400

    ~intent~ional changes

--- app/src/main/java/com/example/doza/uncover/Holla.java
+++ app/src/main/java/com/example/doza/uncover/Holla.java
@@ -1,22 +1,88 @@
 package com.example.doza.uncover;
 
 import android.content.Intent;
+import android.location.Location;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Holla extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
+
+public class Holla extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener{
+    String location = "";
+    String sent="";
+    private GoogleApiClient c = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_holla);
         getIntent().getData();
+        if (c == null) {
+            c = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
+    @Override
+    protected void onStart(){
+        c.connect();
+        super.onStart();
+    }
+    @Override
+    protected void onStop(){
+        c.disconnect();
+        super.onStop();
+    }
+    public void findFriend(){
+        String input = ((EditText)findViewById(R.id.locField)).getText().toString();
+        String lat = input.substring(0,input.indexOf(","));
+        String lon = input.substring(input.indexOf(",")+1,input.length());
+
+    }
+
+
+
     public void toMain(View v){
         Intent X = new Intent(this, MainActivity.class);
 
         startActivity(X);
     }
+    public void sendLoc(View v){
+        Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+        sendIntent.setData(Uri.parse("sms:"));
+        sendIntent.putExtra("sms_body", location);
+        startActivity(sendIntent);
+    }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        try {
+            Location loc = LocationServices.FusedLocationApi.getLastLocation(c);
+            location = loc.getLatitude() + ", " + loc.getLongitude();
+            ((TextView) findViewById(R.id.statText)).setText("location ready to send");
+
+        }catch (SecurityException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
--- app/src/main/java/com/example/doza/uncover/MainActivity.java
+++ app/src/main/java/com/example/doza/uncover/MainActivity.java
@@ -5,13 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
     public void toHolla(View v){
         Intent X = new Intent(this, Holla.class);
 
